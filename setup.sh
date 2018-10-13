ec174826cdf2d793ea4f4a80757d878de0b3275a
Karl Gustav Røksund <kkgg84+git@gmail.com>
2018-10-13 16:35:55 +0200

    Cleaned up the setup.sh file
    Make it easier to read

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ set -e
 
 SERVER=${1:-screen}
 
-CMD="tar xzf - -C /tmp/ \
- && echo Making directory \
- && sudo mkdir -p /srv/smart-screen/ \
- && echo Copying service \
- && sudo cp /tmp/smart-screen.service /srv/smart-screen/ \
- && echo Enabling service \
- && sudo systemctl enable /srv/smart-screen/smart-screen.service \
-"
+CMD='tar xzf - -C /tmp/
+ && echo ≫ Making directory
+ && sudo mkdir -p /srv/smart-screen/
+ && echo ≫ Copying service
+ && sudo cp /tmp/smart-screen.service /srv/smart-screen/
+ && echo ≫ Enabling service
+ && sudo systemctl enable /srv/smart-screen/smart-screen.service
+'
 
 echo 'Running command on "'${SERVER}'":' $CMD
 tar czf - smart-screen.service |ssh $SERVER $CMD
